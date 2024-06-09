cb169512b416fe518ac1321b9a7b3e566da8a8e9
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-06-09 20:40:46 +0500

    fix in urls.py

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,6 +3,6 @@ from snippets import views
 
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
+    path("snippets/", views.snippet_list),
     path("snippets/<int:pk>/", views.snippet_detail),
 ]

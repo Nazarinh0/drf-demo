57384a190bf509c4258beff2eb70dc7789d5f371
Nazar Annakhalov <nazar.annahalov@yandex.com>
2024-06-09 20:39:45 +0500

    fix in serializers.py

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -20,10 +20,10 @@ class SnippetSerializer(serializers.Serializer):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
+        instance.title = validated_data.get("title", instance.title)
+        instance.code = validated_data.get("code", instance.code)
+        instance.linenos = validated_data.get("linenos", instance.linenos)
+        instance.language = validated_data.get("language", instance.language)
+        instance.style = validated_data.get("style", instance.style)
         instance.save()
         return instance
